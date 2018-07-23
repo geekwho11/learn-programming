c501443a196de377a4228b0439a3b34106edf57c
GeekWho <geekwho@xbc.me>
2018-07-23 20:35:49 +0800

    + add data structrue map

---
+++ app/business/DataStructrue/Map/HashMap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @Author: GeekWho
+ * @Date:   2018-07-15 21:42:32
+ * @Last Modified by:   GeekWho
+ * @Last Modified time: 2018-07-23 20:35:35
+ */
+
+namespace DataStructrue\Map;
+
+class HashMap extends \DataStructrue\Base
+{
+}
