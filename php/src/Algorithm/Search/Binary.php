4a6a06ba59a6c7481aedb8b1e040f44587d2f0e6
GeekWho <geekwho@xbc.me>
2018-12-14 15:53:29 +0000

    * 更新二分法查找的单元测试

--- php/src/Algorithm/Search/Binary.php
+++ php/src/Algorithm/Search/Binary.php
@@ -4,27 +4,13 @@
  * @Author: GeekWho
  * @Date:   2018-07-22 10:51:33
  * @Last Modified by:   GeekWho
- * @Last Modified time: 2018-07-22 11:33:59
+ * @Last Modified time: 2018-12-14 23:48:51
  */
 namespace Algorithm\Search;
 
 class Binary extends Base
 {
-    public static function run($num = 10)
-    {
-        $random = self::getRandomData($num);
-        $random = \Algorithm\Sort\QuickSort::sort($random);
-        $findme = mt_rand(0, $num - 1);
-        $findme = $random[$findme];
-        $begin  = microtime(true);
-        $data   = self::search($random, $findme);
-        $end    = microtime(true);
-        $time   = $end - $begin;
-        echo "num $num search $findme cost time is $time s" . PHP_EOL;
-        //self::echoData($random) . PHP_EOL;
-    }
-
-    public static function search($array, $e)
+    public function run($array, $e)
     {
         $start = 0;
         $end   = count($array);
@@ -39,5 +25,19 @@ class Binary extends Base
                 $start= $find + 1;
             }
         }
+    }
+
+    public function test($num = 10)
+    {
+        $random = self::getRandomData($num);
+        $random = (new \Algorithm\Sort\QuickSort)->run($random);
+        $findme = mt_rand(0, $num - 1);
+        $findme = $random[$findme];
+        $begin  = microtime(true);
+        $data   = $this->run($random, $findme);
+        $end    = microtime(true);
+        $time   = $end - $begin;
+        echo "num $num search $findme cost time is $time s" . PHP_EOL;
+        self::echoData($random) . PHP_EOL;
     }
 }
---
+++ php/src/Algorithm/Search/BinaryTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @Author: GeekWho
+ * @Date:   2018-12-14 17:48:03
+ * @Last Modified by:   GeekWho
+ * @Last Modified time: 2018-12-14 23:52:55
+ */
+namespace Algorithm\Search;
+
+class BinaryTest extends \PHPUnit\Framework\TestCase
+{
+    public function testRun()
+    {
+        $tests = [
+            'case1' => [
+                'input' => [
+                    'nums'   => [18,19,37,57,63,68,71,81,84,99],
+                    'findme' => '63'
+                ],
+                'output' => 4,
+            ],
+            'case2' => [
+                'input' => [
+                    'nums'   => [18,19,37,57,63,68,71,81,84,99],
+                    'findme' => '1'
+                ],
+                'output' => false,
+            ],
+        ];
+        foreach ($tests as $key => $case) {
+            $input  = $case['input'];
+            $output = $case['output'];
+            $return = (new \Algorithm\Search\Binary())->run($input['nums'], $input['findme']);
+            $this->assertEquals(
+                true,
+                $return == $output,
+                "run test $key failed. return $return output $output"
+            );
+        }
+
+        $debug = false;
+        if($debug){
+            (new \Algorithm\Search\Binary)->test();
+        }
+    }
+}
