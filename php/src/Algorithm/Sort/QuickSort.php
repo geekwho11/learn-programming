12ce3607f78e8a4ae198eca4006d90785b0d0f25
GeekWho <geekwho@xbc.me>
2018-12-14 10:49:10 +0000

    * 优化快速排序的单元测试

--- php/src/Algorithm/Sort/QuickSort.php
+++ php/src/Algorithm/Sort/QuickSort.php
@@ -4,25 +4,19 @@
  * @Author: GeekWho
  * @Date:   2018-07-21 14:00:11
  * @Last Modified by:   GeekWho
- * @Last Modified time: 2018-07-21 14:30:25
+ * @Last Modified time: 2018-12-14 18:39:02
  */
 namespace Algorithm\Sort;
 
 class QuickSort extends \Algorithm\Sort\Base
 {
-    public static function run($num = 10)
-    {
-        $begin  = microtime(true);
-        $random = self::getRandomData($num);
-        $data   = self::sort($random);
-        $end    = microtime(true);
-        $time   = $end - $begin;
-        echo "num $num sort cost time is $time s" . PHP_EOL;
-        //self::echoData($random) . PHP_EOL;
-        //self::echoData($data) . PHP_EOL;
-    }
-
-    public static function sort(array $data)
+    /**
+     * 1. 注意循环结束条件
+     * 2. 快排的主要思想就是2分法，取一个中间值，将比中间值大和小的值分组。
+     * 3. 然后递归调用，直到触发循环结束条件。
+     *
+     */
+    public function run(array $data)
     {
         $count = count($data);
         if ($count <= 1) {
@@ -38,9 +32,21 @@ class QuickSort extends \Algorithm\Sort\Base
                 $right[] = $data[$i];
             }
         }
-        $left   = self::sort($left);
-        $right  = self::sort($right);
+        $left   = $this->run($left);
+        $right  = $this->run($right);
         $result = array_merge($left, array($k), $right);
         return $result;
+    }
+
+    public function sort($num = 10)
+    {
+        $begin  = microtime(true);
+        $random = self::getRandomData($num);
+        $data   = $this->run($random);
+        $end    = microtime(true);
+        $time   = $end - $begin;
+        echo "num $num sort cost time is $time s" . PHP_EOL;
+        self::echoData($random) . PHP_EOL;
+        self::echoData($data) . PHP_EOL;
     }
 }
