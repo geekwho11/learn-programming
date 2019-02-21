7382a58ed12b8ed14af1b74cb76f885dcbabc51f
GeekWho <geekwho@xbc.me>
2019-02-21 13:58:48 +0800

    + 添加 haproxy Api.php

---
+++ linux/haproxy/Api.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @Author: GeekWho
+ * @Date:   2019-02-21 11:14:11
+ * @Last Modified by:   GeekWho
+ * @Last Modified time: 2019-02-21 12:19:39
+ */
+class Api
+{
+    public function run()
+    {
+        $req = isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:'--';
+        echo sprintf(
+            "request HTTP_HOST %s real SERVER_PORT %s" . PHP_EOL ,
+            $_SERVER['HTTP_HOST'],
+            $_SERVER['SERVER_PORT']
+        );
+    }
+}
+(new \Api)->run();
---
+++ linux/haproxy/run.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Program:
+#.   一键执行脚本
+# History:
+# @Author: GeekWho
+# @Date:   2019-02-21 14:32:43
+# @Last Modified by:   GeekWho
+# @Last Modified time: 2019-02-22 16:18:46
+php=$(which "php")
+if [ -z $php ]; then
+    echo '请先安装php'
+    exit
+fi
+
+docker=$(which "docker")
+if [ -z $docker ]; then
+    echo '请先安装docker'
+    exit
+fi
+
+git=$(which "git")
+if [ -z $git ]; then
+    echo '请先安装git'
+    exit
+fi
+webroot=$(cd "$(dirname "$0")";pwd)
+
+php -S 0.0.0.0:2016 -t $webroot $webroot/Api.php
+php -S 0.0.0.0:2017 -t $webroot $webroot/Api.php
+php -S 0.0.0.0:2018 -t $webroot $webroot/Api.php
+
+ip=$("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'|grep -v '172*'| tail -1")
+if [ -z $ip ]; then
+    echo '获取ip地址失败'
+    exit
+fi
+
+root=$(cd "$(dirname "$0")";cd ..;cd..;pwd)
+cd $root
+if [[ -d docker.xbc.me ]]; then
+  git clone git@github.com:geekwho11/docker.xbc.me.git
+fi
+
+cd docker.xbc.me/docker/alpine
+
+sed -i "s/192.168.1.3/${ip}/g" `grep 192.168.1.3 -rl ./build/haproxy/*`
+
+docker build -t my:haproxy -f build/haproxy/Dockerfile .
+
+docker run -it --rm --name haproxy-syntax-check my:haproxy haproxy -c -f /usr/local/etc/haproxy/haproxy.cfg
+
+docker run -d --name haproxy -p 2019:2019 my:haproxy
+
+# sleep 11
+secs=$((1 * 11))
+while [ $secs -gt 0 ]; do
+   echo -ne "waiting for $secs\033[0K\r"
+   sleep 1
+   : $((secs--))
+done
+
+curl http://0.0.0.0:2019
+
+curl http://0.0.0.0:2019
+
+curl http://0.0.0.0:2019
